c13d184daeddb11470e194b6844a79458ebdd37a
jhonatanT04 <venotacu@gmail.com>
2025-11-25 22:23:04 +0000

    Eliminar front de angular

---
+++ AppEscritorio/mainwindow.h
@@ -0,0 +1,43 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QImage>
+#include <QPixmap>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QSlider>
+#include <opencv2/opencv.hpp>
+#include "ProcesadoIMGc/CTProcessorSimple.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();       // Cargar Imagen
+    void on_pushButton_2_clicked();     // Pipeline interno (botón opcional)
+    void on_pushButton_3_clicked();     // Aplicar Red
+
+    void updateFilters();               // Actualizar sliders en tiempo real
+
+private:
+    Ui::MainWindow *ui;
+    CTImageProcessor *processor;        // Procesador de imágenes
+    cv::Mat currentImage;               // Imagen original
+    cv::Mat pipelineImage;              // Imagen con pipeline interno
+
+    QImage matToQImage(const cv::Mat &mat);
+
+    void applyInternalPipeline();       // Aplica filtros internos automáticamente
+};
+
+#endif // MAINWINDOW_H
