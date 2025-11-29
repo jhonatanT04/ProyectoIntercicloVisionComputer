80f546df63c6cabcb1b03c2b5bcd56fa965c8cc9
JustinLucero07 <justinluceroxd04@gmail.com>
2025-11-29 17:26:22 -0500

    Se modifica para cargar la imagen

--- AppEscritorio/mainwindow.cpp
+++ AppEscritorio/mainwindow.cpp
@@ -77,11 +77,11 @@ QImage MainWindow::matToQImage(const cv::Mat &mat)
 // ==================== BOTÓN 1: Cargar Imagen ====================
 void MainWindow::on_pushButton_clicked()
 {
-    // QString fileName = QFileDialog::getOpenFileName(
-    //     this, "Seleccionar imagen CT", "",
-    //     "Imagenes (*.png *.jpg *.jpeg *.bmp *.IMA *.dcm)");
+    QString fileName = QFileDialog::getOpenFileName(
+         this, "Seleccionar imagen CT", "",
+         "Imagenes (*.png *.jpg *.jpeg *.bmp *.IMA *.dcm)");
 
-    QString fileName = "/home/jhonatan/VisualCodeStudio/ProyectoIntercicloVisionComputer/AppEscritorio/aaa/build/L19.IMA";
+    //QString fileName = "/home/jhonatan/VisualCodeStudio/ProyectoIntercicloVisionComputer/AppEscritorio/aaa/build/L19.IMA";
 
     if(fileName.isEmpty()) return;
 
